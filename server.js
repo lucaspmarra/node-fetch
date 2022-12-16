98a9e6f5cc7495740eccba3e8f6b6eccb81700eb
Lucas Marra <marralucas1@gmail.com>
2022-12-16 09:34:13 -0300

    FEAT: update port for railway

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+// const port = 3001;
 
 app.use(cors());
 
